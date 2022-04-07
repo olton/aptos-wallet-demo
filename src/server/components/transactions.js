738c18f86afdd86c04a0c58db237d0cda7b917e4
Sergey Pimenov <sergey@pimenov.com.ua>
2022-04-07 18:33:15 +0300

    ...

--- src/server/components/transactions.js
+++ src/server/components/transactions.js
@@ -4,8 +4,10 @@ export const getAddressTransactions = async (address, limit = 25, start = 0) => 
     if (!address) {
         return []
     }
-
-    const rest = new RestClient(config.api.rest)
-
-    return await rest.accountTransactions(address, limit, start)
+    try {
+        const rest = new RestClient(config.api.rest)
+        return await rest.accountTransactions(address, limit, start)
+    } catch (e) {
+        return []
+    }
 }
